6e52b000a87ea14092ba7fa688488020e92fa821
vende11s <wie.rzbinski9@gmail.com>
2026-03-24 18:00:02 +0100

    added 'last removed' feature

--- frontend/scripts/clean.js
+++ frontend/scripts/clean.js
@@ -1,49 +1,64 @@
 PORT = 42690
 
-async function changeDir(id){
-    const res = await fetch('http://localhost:'+PORT+'/change_dir/'+id);
+async function changeDir(id) {
+    const res = await fetch('http://localhost:' + PORT + '/change_dir/' + id);
     update();
 }
 
-async function remove(id){
-    const res = await fetch('http://localhost:'+PORT+'/remove/'+id);
+async function remove(id) {
+    const res = await fetch('http://localhost:' + PORT + '/remove/' + id);
     update();
 }
 
-function update(){
-    fetch('http://localhost:'+PORT+'/get_interface_data')
-    .then(response => response.json())
-    .then(res =>{
-        document.getElementById("pwd").innerHTML = res["current_path"];
-        document.getElementById("removed-label").innerHTML = "Removed " + res["removed_label"];
-        document.getElementById("this-info").innerHTML = res["dir_info"];
+function update() {
+    fetch('http://localhost:' + PORT + '/get_interface_data')
+        .then(response => response.json())
+        .then(res => {
+            document.getElementById("pwd").innerHTML = res["current_path"];
+            document.getElementById("removed-label").innerHTML = "Removed " + res["removed_label"];
+            document.getElementById("this-info").innerHTML = res["dir_info"];
 
-        removed_percent = res["removed_percent"]
-        document.getElementById("removed-bar").style=`width:${removed_percent}%;`;
-        document.getElementById("removed-foot").innerHTML=removed_percent + "%" + " cleaned";
+            removed_percent = res["removed_percent"]
+            document.getElementById("removed-bar").style = `width:${removed_percent}%;`;
+            document.getElementById("removed-foot").innerHTML = removed_percent + "%" + " cleaned";
 
-        let filesContainer = document.getElementById("files-container");
-        filesContainer.innerHTML = ""
-        res["files"].forEach(file => {
-            console.log(file)
-            let item = document.createElement("div");
-            item.id = file.id;
-            item.className="file-item";
-            item.tabIndex="0";
+            let filesContainer = document.getElementById("files-container");
+            filesContainer.innerHTML = ""
+            res["files"].forEach(file => {
+                console.log(file)
+                let item = document.createElement("div");
+                item.id = file.id;
+                item.className = "file-item";
+                item.tabIndex = "0";
 
-            item.innerHTML = `
+                item.innerHTML = `
                 <img src="resources/${file.type}.png" class="file-icon">
                 <div class="file-title">${file.name}</div>
                 <div class="file-size">${file.size}</div>
                 <div class="file-date">${file.date_label}</div>
             `;
 
-            item.addEventListener('dblclick', () => {
-                changeDir(item.id);
+                item.addEventListener('dblclick', () => {
+                    changeDir(item.id);
+                });
+                filesContainer.appendChild(item);
             });
-            filesContainer.appendChild(item);
+
+            let deletedContainer = document.getElementById("deleted-container");
+            if (res["last_removed"]) {
+                deletedContainer.innerHTML = "";
+                res["last_removed"].forEach(item => {
+                    let deletedItem = document.createElement("div");
+                    deletedItem.className = "deleted-item";
+                    deletedItem.innerHTML = `
+                    <img src="resources/${item.type}.png" class="file-icon">
+                    <div class="deleted-item-name">${item.path}  </div>
+                    <div class="deleted-item-size">  ${item.size_label}</div>
+                `;
+                    deletedContainer.appendChild(deletedItem);
+                });
+            }
         });
-    });
 }
 
 window.onload = () => {
@@ -54,10 +69,10 @@ window.onload = () => {
 }
 
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'Delete') { // check for Delete key
-    const focused = document.activeElement; // get the currently focused element
-    if (focused) {
-      remove(focused.id);
+    if (event.key === 'Delete') { // check for Delete key
+        const focused = document.activeElement; // get the currently focused element
+        if (focused) {
+            remove(focused.id);
+        }
     }
-  }
 });
